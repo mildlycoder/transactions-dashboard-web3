0b47ab314b59c75163c0ac9e60add674bcb45e6d
Pranav K. <pranavismic@gmail.com>
2024-05-22 14:46:58 +0530

    added new design to transaction card

--- src/app/compononets/dashboard-components/transactionCard.tsx
+++ src/app/compononets/dashboard-components/transactionCard.tsx
@@ -7,10 +7,26 @@ export default function TransactionCard(props: TransactionCardProps) {
 
     const { transaction } = props;
     return (
-        <div className="bg-blue-400 rounded-sm m-5 p-5">
-            <h1 className="text-xl">sender address: {transaction.from} </h1>
-            <h1 className="text-xl">reciever address: {transaction.to} </h1>
-            <h1 className="text-xl">value: {transaction.value} </h1>
+        <div className="bg-blue-400 bg-gradient-to-r from-blue-600 to-violet-600 rounded-md mx-auto p-5 w-[95%] lg:w-[60%] flex flex-col gap-5">
+            <div className="flex items-center gap-3">
+                <h1 className="text-md flex flex-col">
+                <span>From</span>
+                    {transaction.from}
+                </h1>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                    </svg>
+                </div>
+                <h1 className="text-md flex flex-col">
+                <span>To</span>
+                    {transaction.to}
+                </h1>
+            </div>
+            <h1 className="text-xl flex flex-col">
+                value
+               <span className="text-3xl">{transaction.value}</span>
+            </h1>
         </div>
     );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,15 +21,122 @@ const getData = async () => {
 }
 
 export default async function Dashboard() {
-    const response = await getData()
-    console.log(response)
+    /*  const response = await getData() */
+    const list = [
+        {
+            blockNumber: '18971034',
+            timeStamp: '1704823007',
+            hash: '0x0261b59ef5ffae43e73398eda68b926927b686320db430cd5342bc8b5d5fef60',
+            nonce: '2',
+            blockHash: '0xbb09f418604e5dfae4020f3fb38a3d081f93ddc6bd71a0105fcb8435835991ee',
+            transactionIndex: '161',
+            from: '0xa83114a443da1cecefc50368531cace9f37fcccb',
+            to: '0xa8c62111e4652b07110a0fc81816303c42632f64',
+            value: '27278630897672930',
+            gas: '21000',
+            gasPrice: '22139128176',
+            isError: '0',
+            txreceipt_status: '1',
+            input: '0x',
+            contractAddress: '',
+            cumulativeGasUsed: '14607928',
+            gasUsed: '21000',
+            confirmations: '953271',
+            methodId: '0x',
+            functionName: ''
+        }, {
+            blockNumber: '18971034',
+            timeStamp: '1704823007',
+            hash: '0x0261b59ef5ffae43e73398eda68b926927b686320db430cd5342bc8b5d5fef60',
+            nonce: '2',
+            blockHash: '0xbb09f418604e5dfae4020f3fb38a3d081f93ddc6bd71a0105fcb8435835991ee',
+            transactionIndex: '161',
+            from: '0xa83114a443da1cecefc50368531cace9f37fcccb',
+            to: '0xa8c62111e4652b07110a0fc81816303c42632f64',
+            value: '27278630897672930',
+            gas: '21000',
+            gasPrice: '22139128176',
+            isError: '0',
+            txreceipt_status: '1',
+            input: '0x',
+            contractAddress: '',
+            cumulativeGasUsed: '14607928',
+            gasUsed: '21000',
+            confirmations: '953271',
+            methodId: '0x',
+            functionName: ''
+        }, {
+            blockNumber: '18971034',
+            timeStamp: '1704823007',
+            hash: '0x0261b59ef5ffae43e73398eda68b926927b686320db430cd5342bc8b5d5fef60',
+            nonce: '2',
+            blockHash: '0xbb09f418604e5dfae4020f3fb38a3d081f93ddc6bd71a0105fcb8435835991ee',
+            transactionIndex: '161',
+            from: '0xa83114a443da1cecefc50368531cace9f37fcccb',
+            to: '0xa8c62111e4652b07110a0fc81816303c42632f64',
+            value: '27278630897672930',
+            gas: '21000',
+            gasPrice: '22139128176',
+            isError: '0',
+            txreceipt_status: '1',
+            input: '0x',
+            contractAddress: '',
+            cumulativeGasUsed: '14607928',
+            gasUsed: '21000',
+            confirmations: '953271',
+            methodId: '0x',
+            functionName: ''
+        }, {
+            blockNumber: '18971034',
+            timeStamp: '1704823007',
+            hash: '0x0261b59ef5ffae43e73398eda68b926927b686320db430cd5342bc8b5d5fef60',
+            nonce: '2',
+            blockHash: '0xbb09f418604e5dfae4020f3fb38a3d081f93ddc6bd71a0105fcb8435835991ee',
+            transactionIndex: '161',
+            from: '0xa83114a443da1cecefc50368531cace9f37fcccb',
+            to: '0xa8c62111e4652b07110a0fc81816303c42632f64',
+            value: '27278630897672930',
+            gas: '21000',
+            gasPrice: '22139128176',
+            isError: '0',
+            txreceipt_status: '1',
+            input: '0x',
+            contractAddress: '',
+            cumulativeGasUsed: '14607928',
+            gasUsed: '21000',
+            confirmations: '953271',
+            methodId: '0x',
+            functionName: ''
+        }, {
+            blockNumber: '18971034',
+            timeStamp: '1704823007',
+            hash: '0x0261b59ef5ffae43e73398eda68b926927b686320db430cd5342bc8b5d5fef60',
+            nonce: '2',
+            blockHash: '0xbb09f418604e5dfae4020f3fb38a3d081f93ddc6bd71a0105fcb8435835991ee',
+            transactionIndex: '161',
+            from: '0xa83114a443da1cecefc50368531cace9f37fcccb',
+            to: '0xa8c62111e4652b07110a0fc81816303c42632f64',
+            value: '27278630897672930',
+            gas: '21000',
+            gasPrice: '22139128176',
+            isError: '0',
+            txreceipt_status: '1',
+            input: '0x',
+            contractAddress: '',
+            cumulativeGasUsed: '14607928',
+            gasUsed: '21000',
+            confirmations: '953271',
+            methodId: '0x',
+            functionName: ''
+        },
+    ]
     return (
-        <div className="flex flex-col bg-black min-h-screen">
-            <h1 className="text-2xl text-white"> welcome to web3 tracker</h1>
-            <div className="mt-5">
+        <div className="flex flex-col bg-gradient-to-r from-slate-900 to-slate-700 min-h-screen">
+            <h1 className="text-2xl m-5 p-5 text-white"> welcome to web3 tracker</h1>
+            <div className="flex flex-col gap-5">
                 {
-                    (response.result) &&
-                    response?.result?.map((transaction: ITransaction) => {
+                    (list) &&
+                    list.map((transaction: ITransaction) => {
                         return <TransactionCard key={transaction.hash}
                             transaction={transaction} />;
                     })
